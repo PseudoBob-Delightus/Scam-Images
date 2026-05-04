30336030e1877dade44ca4925722e4b68021f552
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-05-04 23:11:32 +1000

    Big refactor

--- code/build_list.py
+++ code/build_list.py
@@ -6,18 +6,26 @@ import imagehash
 from hamming import hamming_distance
 from PIL import Image
 
+
 # Generates a list of hashes based on a folder "scam_images" with a list of folders containing images within it.
 
 
 def get_phash(filename: str) -> str:
-    output = ""
+    phash: str = ""
     try:
         img = Image.open(filename)
-        output = str(imagehash.phash(img, hash_size=16))
+        phash = str(imagehash.phash(img, hash_size=16))
     finally:
-        return output
+        return phash
 
-def rem_collisions(hashes: list) -> list:
+
+def rem_collisions(hashes: list[str]) -> list[str]:
+    """
+    takes a list of hashes (str) and removes collisions (distance < 4)
+    returns a list[str]
+    """
+    hash1: str
+    hash2: str
     for hash1 in deepcopy(hashes):
         for hash2 in deepcopy(hashes):
             if hash1 == hash2:
@@ -27,12 +35,18 @@ def rem_collisions(hashes: list) -> list:
                 break
     return hashes
 
-def main() -> None:
-    hashes: list = []
+
+def get_hashes() -> list[str]:
     file_list = glob.glob(r"../*/*")
+    hashes: list = []
     for target in file_list:  # Build list
         if phash := get_phash(target):
             hashes.append(phash)
+    return hashes
+
+
+def main() -> None:
+    hashes: list = get_hashes()
     print(f"Pre-dupe-removal:   {len(hashes)}")
     hashes = list(set(hashes))  # Remove dupes
     print(f"Post-dupe-removal:  {len(hashes)}")
