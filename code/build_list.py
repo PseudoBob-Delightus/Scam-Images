60ff230b9ad9a5c4912eb4d5294ade32a4da2c7a
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-07-13 13:20:22 +1000

    Added new image.
    Added support for split images.
    Now searches for images that might be two images (sneaky scammers!) and splits them if so. Will submit combined hash and singular hash.
    
    So, we enter the arms race. I bet I can automate anything you do by hand (:
    
    Bring it.

--- code/build_list.py
+++ code/build_list.py
@@ -5,7 +5,9 @@ from typing import Union
 
 import imagehash
 from cloudflare.types.kv.namespaces import ValueUpdateResponse
+import io
 
+from split_images import split_two_images
 from hamming import hamming_distance
 from PIL import Image
 import os
@@ -15,15 +17,21 @@ from cloudflare import Cloudflare
 # Generates a list of hashes based on a folder "scam_images" with a list of folders containing images within it.
 
 
-def get_phash_and_dimensions(filename: str) -> tuple[str, tuple]:
+def get_phash_and_dimensions(filename: str) -> list[tuple[str, tuple]]:
     phash: str = ""
     dimensions: tuple[int, int] = (0,0)
+    output: list[tuple[str, tuple]] = []
     try:
         img = Image.open(filename)
-        phash = str(imagehash.phash(img, hash_size=16))
-        dimensions = img.size
+        image = io.BytesIO()
+        img.save(image, format="PNG")
+        images = split_two_images(image)
+        for image in images:
+            phash = str(imagehash.phash(Image.open(image), hash_size=16))
+            dimensions = img.size
+            output.append((phash, dimensions))
     finally:
-        return phash, dimensions
+        return output
 
 
 def rem_collisions(hashes: list[str]) -> list[str]:
@@ -48,17 +56,19 @@ def get_filenames() -> list[str]:
 def get_hashes() -> list[str]:
     hashes: list = []
     for target in get_filenames():  # Build list
-        phash, _ = get_phash_and_dimensions(target)
-        if phash:
-            hashes.append(phash)
+        result = get_phash_and_dimensions(target)
+        for phash, _ in result:
+            if phash:
+                hashes.append(phash)
     return hashes
 
 def get_hashes_and_dimensions() -> list[dict]:
     hashes_and_dimensions: list[dict] = []
     for target in get_filenames():  # Build list
-        phash, dimensions = get_phash_and_dimensions(target)
-        if phash and dimensions != (0,0):
-            hashes_and_dimensions.append({"phash": phash, "dimensions": dimensions})
+        result = get_phash_and_dimensions(target)
+        for phash, dimensions in result:
+            if phash and dimensions != (0,0):
+                hashes_and_dimensions.append({"phash": phash, "dimensions": dimensions})
     return hashes_and_dimensions
 
 
@@ -94,6 +104,7 @@ def main() -> None:
         print(f"New hashes\nCount: {len(hashes)} List: {hash_string}")
     except OSError:
         old_hashes = []
+        old_hash_string = ""
     finally:
         with open("../hashes.json", "w") as hash_file:
             json.dump(hashes, hash_file, ensure_ascii=False, indent=4)
@@ -105,6 +116,7 @@ def main() -> None:
         print(f"New old_hashes_and_dimensions\nCount: {len(hashes_and_dimensions)} List: {hashes_and_dimensions_string}")
     except OSError:
         old_hashes_and_dimensions = []
+        old_hashes_and_dimensions_string = ""
     finally:
         with open("../hashes_and_dimensions.json", "w") as hash_file:
             json.dump(hashes_and_dimensions, hash_file, ensure_ascii=False, indent=4)
@@ -114,37 +126,69 @@ def main() -> None:
     with open("../.kv_config.json", "r") as kv_config_file:
         kv_config = json.load(kv_config_file)
     client = Cloudflare(api_token=kv_config["CLOUDFLARE_API_TOKEN"])
-    response_a = False
-    response_b = False
+    response_a1 = None
+    response_b1 = None
+    response_a2 = ""
+    response_b2 = ""
     if len(old_hashes) < len(hashes):
-        response_a = update_phash(client=client, kv_config=kv_config, hash_string=hash_string)
+        if old_hash_string:
+            response_a1 = update_phash(client=client, kv_config=kv_config, hash_string=old_hash_string, backup=True)
+        else:
+            response_a1 = None
+        response_a2 = update_phash(client=client, kv_config=kv_config, hash_string=hash_string)
     else:
         print("Hash list same or smaller.")
     if len(old_hashes_and_dimensions) < len(hashes_and_dimensions):
-        response_b = update_phash_and_dimensions(client=client, kv_config=kv_config, hashes_and_dimensions_string=hashes_and_dimensions_string)
+        if old_hashes_and_dimensions_string:
+            response_b1 = update_phash_and_dimensions(client=client, kv_config=kv_config, hashes_and_dimensions_string=old_hashes_and_dimensions_string, backup=True)
+        else:
+            response_b1 = None
+        response_b2 = update_phash_and_dimensions(client=client, kv_config=kv_config, hashes_and_dimensions_string=hashes_and_dimensions_string)
     else:
         print("Hash and dimensions list same or smaller.")
 
-    if response_a:
-        print(f"Response: {response_a}")
-    elif response_a is None:
+    if response_a1:
+        print(f"Response: \n{response_a1}")
+    elif response_a2 is None:
         print("Cloudflare kv updated for hash list!")
-    if response_b:
-        print(f"Response: {response_b}")
-    elif response_b is None:
+    if response_a2:
+        print(f"Response: \n{response_a2}")
+    if response_b1:
+        print(f"Response: \n{response_b1}")
+    elif response_b2 is None:
         print("Cloudflare kv updated for hash and dimension list!")
+    if response_b2:
+        print(f"Response: \n{response_b2}")
 
-def update_phash(client: Cloudflare, kv_config: dict, hash_string: str) -> Union[str, None, ValueUpdateResponse]:
+def update_phash(client: Cloudflare, kv_config: dict, hash_string: str, backup: bool = False) -> Union[str, None, ValueUpdateResponse]:
     if not hash_string:
         print("No hash string")
         return "Not updated"
-    return client.kv.namespaces.values.update(key_name=kv_config["KV_PAIR1"], namespace_id=kv_config["NAMESPACE_ID"], value=hash_string.encode('utf-8'), account_id=kv_config["ACCOUNT_ID"])
+    if not backup:
+        return client.kv.namespaces.values.update(key_name=kv_config["KV_PAIR1"],
+                                                  namespace_id=kv_config["NAMESPACE_ID"],
+                                                  value=hash_string.encode('utf-8'),
+                                                  account_id=kv_config["ACCOUNT_ID"])
+    else:
+        return client.kv.namespaces.values.update(key_name=kv_config["KV_PAIR1"] + "_backup",
+                                                  namespace_id=kv_config["NAMESPACE_ID"],
+                                                  value=hash_string.encode('utf-8'),
+                                                  account_id=kv_config["ACCOUNT_ID"])
 
-def update_phash_and_dimensions(client: Cloudflare, kv_config: dict, hashes_and_dimensions_string: str) -> Union[str, None, ValueUpdateResponse]:
+def update_phash_and_dimensions(client: Cloudflare, kv_config: dict, hashes_and_dimensions_string: str, backup: bool = False) -> Union[str, None, ValueUpdateResponse]:
     if not hashes_and_dimensions_string:
         print("No hashes and dimensions string")
         return "Not updated"
-    return client.kv.namespaces.values.update(key_name=kv_config["KV_PAIR2"], namespace_id=kv_config["NAMESPACE_ID"], value=hashes_and_dimensions_string.encode('utf-8'), account_id=kv_config["ACCOUNT_ID"])
+    if not backup:
+        return client.kv.namespaces.values.update(key_name=kv_config["KV_PAIR2"],
+                                                  namespace_id=kv_config["NAMESPACE_ID"],
+                                                  value=hashes_and_dimensions_string.encode('utf-8'),
+                                                  account_id=kv_config["ACCOUNT_ID"])
+    else:
+        return client.kv.namespaces.values.update(key_name=kv_config["KV_PAIR2"] + "_backup",
+                                                  namespace_id=kv_config["NAMESPACE_ID"],
+                                                  value=hashes_and_dimensions_string.encode('utf-8'),
+                                                  account_id=kv_config["ACCOUNT_ID"])
 
 
 if __name__ == "__main__":
---
+++ code/split_images.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+
+import cv2
+import numpy as np
+
+def is_much_wider_than_tall(img: np.ndarray) -> bool:
+    height, width, _ = img.shape
+    return True if (width > (height * 1.4) ) else False
+
+def split_two_images(image: BytesIO) -> list[BytesIO]:
+    output: list[BytesIO] = [image]
+    try:
+        img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), 1)
+        if not is_much_wider_than_tall(img):
+            return output
+        height, width, _ = img.shape
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        start_col = int(width * 0.20)
+        end_col = int(width * 0.80)
+        smoothed_gray = cv2.GaussianBlur(gray, (15, 1), 0)
+        column_brightness = np.mean(smoothed_gray[:, start_col:end_col], axis=0)
+        brightness_gradients = np.abs(np.diff(column_brightness))
+        best_relative_cut = np.argmax(brightness_gradients)
+        cut_x = start_col + best_relative_cut
+        crops = [img[0:height, 0:cut_x], img[0:height, cut_x:width]]
+        output: list[BytesIO] = []
+        for crop in crops:
+            out_image = cv2.imencode(".png", crop)[1].tobytes()
+            output.append(BytesIO(out_image))
+        return output
+    except Exception:
+        return output
